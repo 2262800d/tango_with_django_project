bbf9ef68d629408ef5cd2983609c1ec2307a2af3
Ivan <2262800d@student.gla.ac.uk>
2018-01-25 19:03:53 +0000

    Chapter 6

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR=os.path.join(BASE_DIR, 'templates')
 
 
-STATIC_DIR=os.path.join(BASE_DIR, 'static')
+STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 MEDIA_DIR=os.path.join(BASE_DIR, 'media')
 
